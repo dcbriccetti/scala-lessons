5ed2655735280f136307e2b5dcea50020c43e5f0
Dave Briccetti <daveb@davebsoft.com>
2014-08-14 16:20:59 -0700

    State code lookup example (Map, Option, match)

---
+++ src/StateCodes.scala
@@ -0,0 +1,14 @@
+import scala.io.StdIn._
+
+object StateCodes extends App {
+  val states = Map(
+    "AZ" -> "Arizona",
+    "CA" -> "California"
+  )
+  val code = readLine("Enter a state code ")
+  val opState = states.get(code.toUpperCase)
+  opState match {
+    case Some(state) => println(state)
+    case None        => println("There is no state with that code")
+  }
+}
