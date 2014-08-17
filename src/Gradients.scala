ff9b7d47d8b6055a6de66c774450998961f15290
Dave Briccetti <daveb@davebsoft.com>
2014-08-16 20:15:19 -0700

    Simplify and improve code. Make gradation width 1 (from 3).

--- src/Gradients.scala
+++ src/Gradients.scala
@@ -3,36 +3,39 @@ import scala.swing._
 
 object Gradients extends SimpleSwingApplication {
 
-  def top = new MainFrame { // top is a required method
+  def top = new MainFrame {
     title = "Gradients Demo"
     contents = new GradientsDisplay
+    peer.setLocationRelativeTo(null)
   }
 }
 
 class GradientsDisplay extends Component {
   def constantZero(i: Int) = 0
-  def increment(i: Int) = i
-  def decrement(i: Int) = 255 - i
-  val allFns = Seq(constantZero _, increment _, decrement _)
+  def increment   (i: Int) = i
+  def decrement   (i: Int) = 255 - i
+
+  val gradFuncs = Seq(constantZero _, increment _, decrement _)
   val BarHeight = 20
   val BarSpacing = 3
-  val Margin = 10
+  val GradationWidth = 1
+  val Margin = BarSpacing
 
-  preferredSize = new Dimension(3 * 256 + 2 * Margin, 27 * (BarHeight + BarSpacing) + 2 * Margin)
+  preferredSize = new Dimension(GradationWidth * 256 + 2 * Margin, 27 * BarHeight + 26 * BarSpacing + 2 * Margin)
 
   override protected def paintComponent(g2d: Graphics2D) = {
     super.paintComponent(g2d)
     var y = Margin
 
     val idxCombos = for {
-        i <- 0 to 2
-        j <- 0 to 2
-        k <- 0 to 2
+        r <- gradFuncs
+        g <- gradFuncs
+        b <- gradFuncs
     } {
-        0 to 255 foreach(g => {
-          val x = Margin + 3 * g
-          g2d.setColor(new Color(allFns(i)(g), allFns(j)(g), allFns(k)(g)))
-          g2d.fillRect(x, y, 3, BarHeight)
+        0 to 255 foreach(gradation => {
+          val x = Margin + GradationWidth * gradation
+          g2d.setColor(new Color(r(gradation), g(gradation), b(gradation)))
+          g2d.fillRect(x, y, GradationWidth, BarHeight)
         })
         y += BarHeight + BarSpacing
     }
