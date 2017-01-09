5f03a30e59e3b2b279ee9a4633ad793c7af187e5
Dave Briccetti <daveb@davebsoft.com>
2017-01-09 01:27:43 -0800

    Improve code

--- src/proc/BruteSphere.scala
+++ src/proc/BruteSphere.scala
@@ -1,24 +1,29 @@
 package proc
 
 import java.text.NumberFormat
-
 import processing.core.{PApplet, PConstants, PFont}
 
+/** Graphically demonstrates how to find points which lie near the surface of a sphere,
+  * using randomly chosen x, y, and z coordinates and the Pythagorean Theorem.
+  */
 class BruteSphere extends PApplet {
-  val ScreenWidth = 1440
-  val ScreenHeight = ScreenWidth
-  val radius = ScreenWidth * 0.4
-  val SurfaceThickness = 50
+  val ScreenDimension = 720
+  val SphereRadius = ScreenDimension * 0.4
+  val SurfaceThickness = 32
+  val YRotateRadiansPerMillisecond = 0.0001f
+  val PointsToAddEachFrame = 1000
   var font: PFont = _
   val fmt = NumberFormat.getIntegerInstance
 
-  case class Point(x: Int, y: Int, z: Int, surfaceCloseness: Double)
+  case class Point(x: Int, y: Int, z: Int,
+    surfaceCloseness: Double /* 1 = at surface, 0 = SurfaceThickness pixels inside the surface */)
+
   var points = Vector[Point]()
   var numPointsGenerated = 0
-  var yRot = 0f
+  val startTimeMillis = System.currentTimeMillis
 
   override def settings(): Unit = {
-    size(ScreenWidth, ScreenHeight, PConstants.P3D)
+    size(ScreenDimension, ScreenDimension, PConstants.P3D)
   }
 
   override def setup(): Unit = {
@@ -30,33 +35,38 @@ class BruteSphere extends PApplet {
   override def draw(): Unit = {
     addBatchOfPoints()
     background(0)
+
     pushMatrix()
     translate(width / 2, height / 2, 0)
-    rotateY(yRot)
-    yRot += 0.004f
+    rotateY(YRotateRadiansPerMillisecond * (System.currentTimeMillis - startTimeMillis))
     points foreach { p =>
-      val greenAmt = (p.surfaceCloseness * 256).toInt
-      stroke(0, greenAmt, 256 - greenAmt)
+      val greenAmt = math.round(p.surfaceCloseness * 255) // Greener nearer the surface
+      val blueAmt = 255 - greenAmt // Bluer farther from the surface
+      stroke(0, greenAmt, blueAmt)
       point(p.x, p.y, p.z)
     }
     popMatrix()
+
     textFont(font)
-    text(s"Points generated: ${fmt.format(numPointsGenerated)}", 10, height - 20)
-    text(s"Points on “surface”: ${fmt.format(points.size)}", 220, height - 20)
-    text(s"Frames/second: ${frameRate.toInt}", 420, height - 20)
+    val textY = height - 10
+    text(s"Points generated: ${fmt.format(numPointsGenerated)}", 10, textY)
+    text(s"Points on “surface”: ${fmt.format(points.size)}", 220, textY)
+    text(s"Frames/second: ${frameRate.toInt}", 420, textY)
   }
 
   private def addBatchOfPoints() = {
-    def rp = (math.random * ScreenWidth).toInt - ScreenWidth / 2
+    def randCoord = (math.random * ScreenDimension).toInt - ScreenDimension / 2
 
-    1 to 1000 foreach { i =>
+    1 to PointsToAddEachFrame foreach { _ =>
       numPointsGenerated += 1
-      val x = rp
-      val y = rp
-      val z = rp
-      val distanceFromCenter = math.sqrt(x * x + y * y + z * z)
-      if (distanceFromCenter < radius && distanceFromCenter > radius - SurfaceThickness)
-        points :+= Point(x, y, z, 1.0 - (radius - distanceFromCenter) / SurfaceThickness)
+      val (x, y, z) = (randCoord, randCoord, randCoord)
+      val radiusThisPoint = math.sqrt(x * x + y * y + z * z)
+      val pointWithinSurfaceThickness =
+        radiusThisPoint >= SphereRadius - SurfaceThickness && radiusThisPoint <= SphereRadius
+      if (pointWithinSurfaceThickness) {
+        val surfaceCloseness = 1.0 - (SphereRadius - radiusThisPoint) / SurfaceThickness
+        points :+= Point(x, y, z, surfaceCloseness)
+      }
     }
   }
 }
