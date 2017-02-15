77e59fbb617a49c58c864ac6e5a8f4dcd5a04568
Dave Briccetti <daveb@davebsoft.com>
2017-02-14 17:12:24 -0800

    Use flatMap

--- src/DivisibleProblem.scala
+++ src/DivisibleProblem.scala
@@ -6,26 +6,28 @@ for those divisible by 7, show ÷7 instead.
 
 object Solution1 extends App {
   println(
-    (1 to 100 map(n => {
+    (1 to 100 flatMap (n => {
       def divBy(div: Int) = n % div == 0
+
       (divBy(4), divBy(7)) match {
         case (false, false) => Some(n)
         case (false, true ) => Some("÷7")
         case (true,  false) => Some("÷4")
         case (true,  true ) => None
       }
-    })).flatten.mkString(", ")
+    })).mkString(", ")
   )
 }
 
 object Solution2 extends App {
   println(
-    (1 to 100 map(n => {
+    (1 to 100 flatMap (n => {
       def divBy(div: Int) = n % div == 0
+
       if      (divBy(4) && divBy(7))  None
       else if (divBy(4))              Some("÷4")
       else if (divBy(7))              Some("÷7")
       else                            Some(n)
-    })).flatten.mkString(", ")
+    })).mkString(", ")
   )
 }
