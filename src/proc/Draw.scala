cf4730e26ad0b60b7c2e40b42f7d34891aa4fd7e
Dave Briccetti <daveb@davebsoft.com>
2017-12-12 18:08:12 -0800

    Add some color, and thicken the line

--- src/proc/Draw.scala
+++ src/proc/Draw.scala
@@ -1,8 +1,11 @@
 package proc
-import processing.core.PApplet
+import processing.core.{PApplet, PConstants}
 
 class Draw extends PApplet {
   override def draw() = {
+    colorMode(PConstants.HSB)
+    strokeWeight(4)
+    stroke(frameCount % 360, 256, 256)
     line(pmouseX, pmouseY, mouseX, mouseY)
   }
 }
